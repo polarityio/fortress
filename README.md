59a70f3551cae2ca0baaa4a7f67de57f3ab2b40b
radar989 <joem@polarity.io>
2021-04-02 10:29:50 -0400

    Fortress looks up SHA256's against fortress's file intergrity system.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Polarity Fortress Integration
+
+The Polarity Fortress integration allows for SHA-256 entity types to be enriched against the Fortress File Integrity Assurance (FIA) API.
+
+For more information on Fortress, please visit https://fortressinfosec.com/
+
+## Installation Instructions
+
+Installation instructions for integrations are provided on the [PolarityIO GitHub Page](https://polarityio.github.io/).
+
+## Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see:
+
+https://polarity.io/
---
+++ config/config.js
@@ -0,0 +1,95 @@
+module.exports = {
+  /**
+   * Name of the integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @required
+   */
+  name: 'Fortress FIA Hash Search',
+  /**
+   * The acronym that appears in the notification window when information from this integration
+   * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
+   * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
+   * here will be carried forward into the notification window.
+   *
+   * @type String
+   * @required
+   */
+  acronym: 'FIA',
+  /**
+   * Description for this integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @optional
+   */
+  description: 'Fortress File Integrity Assurance provides real-time patch source verification and patch integrity validation, providing another layer of protection for software updates.',
+  entityTypes: ['SHA256'],
+  onDemandOnly: true,
+  defaultColor: 'light-gray',
+  /**
+   * Provide custom component logic and template for rendering the integration details block.  If you do not
+   * provide a custom template and/or component then the integration will display data as a table of key value
+   * pairs.
+   *
+   * @type Object
+   * @optional
+   */
+  styles: ['./styles/style.less'],
+  block: {
+    component: {
+      file: './components/block.js'
+    },
+    template: {
+      file: './templates/block.hbs'
+    }
+  },
+  request: {
+    // Provide the path to your certFile. Leave an empty string to ignore this option.
+    // Relative paths are relative to the Urlhaus integration's root directory
+    cert: '',
+    // Provide the path to your private key. Leave an empty string to ignore this option.
+    // Relative paths are relative to the Urlhaus integration's root directory
+    key: '',
+    // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+    // Relative paths are relative to the Urlhaus integration's root directory
+    passphrase: '',
+    // Provide the Certificate Authority. Leave an empty string to ignore this option.
+    // Relative paths are relative to the Urlhaus integration's root directory
+    ca: '',
+    // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+    // the url parameter (by embedding the auth info in the uri)
+    proxy: '',
+
+    rejectUnauthorized: false
+  },
+  logging: {
+    level: 'info' //trace, debug, info, warn, error, fatal
+  },
+  /**
+   * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
+   * as an array of option objects.
+   *
+   * @type Array
+   * @optional
+   */
+   options: [
+     {
+       key: 'url',
+       name: 'Base Fortress FIA URL',
+       description: 'The base URL for the Fortress FIA server including the schema (i.e., https://)',
+       default: '',
+       type: 'text',
+       userCanEdit: false,
+       adminOnly: true
+     },
+     {
+       key: "apiKey",
+       name: "Valid Fortress FIA API Key",
+       description: "Valid Fortress FIA API Key",
+       default: '',
+       type: "password",
+       userCanEdit: true,
+       adminOnly: false
+     }
+   ]
+};
